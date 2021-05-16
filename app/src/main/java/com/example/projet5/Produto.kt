57d741f77ea96bf84d366cbbdc615a217dfa0d6e
Cristiano <cris141187@gmail.com>
2021-05-16 14:38:53 -0300

    versao01

--- app/src/main/java/com/example/projet5/Produto.kt
+++ app/src/main/java/com/example/projet5/Produto.kt
@@ -1,7 +1,9 @@
 package com.example.projet5
+
 import android.graphics.Bitmap
 
 data class Produto (
+    val id: Int,
     val nome: String,
     val quantidade: Int,
     val valor: Double,
--- app/src/main/java/com/example/projet5/Utils.kt
+++ app/src/main/java/com/example/projet5/Utils.kt
@@ -1,5 +1,22 @@
 package com.example.projet5
-    val produtosGlobal = mutableListOf<Produto>()
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import java.io.ByteArrayOutputStream
+
+val produtosGlobal = mutableListOf<Produto>()
+
+fun Bitmap.toBYteArray(): ByteArray {
+    val stream = ByteArrayOutputStream()
+    //comprimindo a imagem
+    this.compress(android.graphics.Bitmap.CompressFormat.PNG, 0, stream)
+    //transformando em um array de caracteres
+    return stream.toByteArray()
+}
+
+fun ByteArray.toBitmap(): Bitmap {
+    return BitmapFactory.decodeByteArray(this, 0, this.size)
+}
 
 
 
